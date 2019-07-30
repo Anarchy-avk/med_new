4b5c8bac1395985b6ddb7b71b5f2b3027b6fa6a4
Anarchy <anarchy.avk@gmail.com>
2019-07-30 13:39:55 +0300

    Подправлен вид талона

--- resources/views/widget/talon.blade.php
+++ resources/views/widget/talon.blade.php
@@ -1,7 +1,7 @@
 <style>
     h3 {
         text-transform: uppercase;
-        font-size: 16px !important;
+        font-size: 14px !important;
         text-align: center;
         line-height: 1 !important;
         margin: 10px 0px;
@@ -31,36 +31,37 @@
         text-align: left;
     }
 </style>
+
 <div class="step3" style="display: block;">
-    <h3>Ваш талон</h3>
-    <div class="success">
-
-        <h2>Заказ №<span id="order">{{ $order }}</span></h2>
-
-        <table>
-            <tr>
-                <td><label class="info_title">Адрес:</label></td>
-                <td>
-                    <div class="address item_result">{{ $filial }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td><label class="info_title">Специальность:</label></td>
-                <td>
-                    <div id="specialityText" class="spec item_result">{{ $spec }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td><label class="info_title">Дата и время:</label></td>
-                <td>
-                    <div class="date-time item_result">{{ $time }}</div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="{{ $qRcode }}" />
-                </td>
-            </tr>>
-        </table>
-    </div>
+    <img class="alignnone size-full" src="{{ asset('img/talon-logo-top.png') }}" alt="">
+    <br/>
+    <h2>Заказ №<span id="order">{{ $order }}</span></h2>
+    <h3>Талон на прием к врачу</h3>
+    <table>
+        <tr>
+            <td><label class="info_title">Адрес:</label></td>
+            <td>
+                <div class="address item_result">{{ $filial }}</div>
+            </td>
+        </tr>
+        <tr>
+            <td><label class="info_title">Специальность:</label></td>
+            <td>
+                <div id="specialityText" class="spec item_result">{{ $spec }}</div>
+            </td>
+        </tr>
+        <tr>
+            <td><label class="info_title">Дата и время:</label></td>
+            <td>
+                <div class="date-time item_result">{{ $time }}</div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="{{ $qRcode }}" />
+            </td>
+        </tr>>
+    </table>
+    <br/>
+    <img class="alignnone size-full" src="{{ asset('img/talon-logo-bottom.png') }}" alt="">
 </div>
