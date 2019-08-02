b915a4d5016bf1a37d3182c5e7473cf796b990f6
Anarchy <anarchy.avk@gmail.com>
2019-08-02 10:39:38 +0300

    Исправлен layot с данными клиентов

--- resources/views/widget/index.blade.php
+++ resources/views/widget/index.blade.php
@@ -136,111 +136,104 @@
                                     </div>
                                     <meta name="_token" content="{{ csrf_token() }}"/>
                                     <form id="customer" id="sign-up" action="/client" method="post">
-                                        <div class="row" id="cust-row">
-                                            <div class="col-sm-6" style="padding-left:5px;">
-                                                <div class="form-group">
+                                        <div class="row" id="cust-row" style="padding-left: 5px; padding-right: 5px;">
+                                            <div class="row">
+                                                <div class="form-group col-sm-6">
                                                     <label for="surname" class="label_client">Фамилия</label>
                                                     <input placeholder=" " name="surname" id="surname" type="text"
                                                            class="form-control" value="{{ $last_name }}">
                                                 </div>
-                                                
-                                                <div class="form-group">
-                                                    <label for="patronymic" class="label_client">Отчество</label>
-                                                    <input placeholder="" name="patronymic" id="patronymic" type="text"
-                                                           class="form-control" value="{{ $patronymic }}">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="phone" class="label_client">Телефон</label>
-                                                    <input placeholder="" name="phone" id="phone" type="text"
-                                                           class="form-control" value="{{ $phone }}">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6" style="padding-right:5px;">
-                                                <div class="form-group">
+                                                <div class="form-group col-sm-6">
                                                     <label for="name" class="label_client">Имя</label>
                                                     <input placeholder="" name="name" id="name" type="text"
                                                            class="form-control" value="{{ $first_name }}" style="margin-bottom: 21px;">
                                                 </div>
-                                                
-                                                
-                                                <div class="form-group" id="cust-form">
+                                            </div>
+                                            <div class="row">
+                                                <div class="form-group col-sm-6">
+                                                    <label for="patronymic" class="label_client">Отчество</label>
+                                                    <input placeholder="" name="patronymic" id="patronymic" type="text"
+                                                           class="form-control" value="{{ $patronymic }}">
+                                                </div>
+                                                <div class="form-group col-sm-6" id="cust-form">
                                                     <label for="years" class="label_client">Ваш день рождения</label> <!-- <br> -->
                                                     <div class="cust-lable" style="margin-bottom: 15px;">
-                                                    <label></label>
-                                                    <select name="year" id="year" style="width: 65px; padding: 8px;text-align: center;" required>
-                                                        <?php 
+                                                        <label></label>
+                                                        <select name="year" id="year" style="width: 65px; padding: 8px;text-align: center;" required>
+                                                            <?php
                                                             $year = date("Y");
                                                             for ($i = $year; $i >= ($year - 120) ; $i--) {
-                                                        ?>
-                                                        <option value={{ $i }}>{{ $i }}</option>
-                                                        <?php } ?>
-                                                    </select>
-                                                    <label></label>
-                                                    <select name="month" id="month" style="width: 65px;padding: 8px;text-align: center;" required>
-
-                                                        <option value=01>01</option>
-                                                        <option value=02>02</option>
-                                                        <option value=03>03</option>
-                                                        <option value=04>04</option>
-                                                        <option value=05>05</option>
-                                                        <option value=06>06</option>
-                                                        <option value=07>07</option>
-                                                        <option value=08>08</option>
-                                                        <option value=09>09</option>
-                                                        <option value=10>10</option>
-                                                        <option value=11>11</option>
-                                                        <option value=12>12</option>
-
-                                                      </select>
-                                            
-                                                    <label></label>
-                                                    <select name="days" id="days" style="width: 65px; padding: 8px;text-align: center;" required>  
-
-                                                        <option value=01>01</option>
-                                                        <option value=02>02</option>
-                                                        <option value=03>03</option>
-                                                        <option value=04>04</option>
-                                                        <option value=05>05</option>
-                                                        <option value=06>06</option>
-                                                        <option value=07>07</option>
-                                                        <option value=08>08</option>
-                                                        <option value=09>09</option>
-                                                        <option value=10>10</option>
-                                                        <option value=11>11</option>
-                                                        <option value=12>12</option>
-                                                        <option value=13>13</option>
-                                                        <option value=14>14</option>
-                                                        <option value=15>15</option>
-                                                        <option value=16>16</option>
-                                                        <option value=17>17</option>
-                                                        <option value=18>18</option>
-                                                        <option value=19>19</option>
-                                                        <option value=20>20</option>
-                                                        <option value=21>21</option>
-                                                        <option value=22>22</option>
-                                                        <option value=23>23</option>
-                                                        <option value=24>24</option>
-                                                        <option value=25>25</option>
-                                                        <option value=26>26</option>
-                                                        <option value=27>27</option>
-                                                        <option value=28>28</option>
-                                                        <option value=29>29</option>
-                                                        <option value=30>30</option>
-                                                        <option value=31>31</option>
-
-
-                                                      </select>
-                                                      </div>
+                                                            ?>
+                                                            <option value={{ $i }}>{{ $i }}</option>
+                                                            <?php } ?>
+                                                        </select>
+                                                        <label></label>
+                                                        <select name="month" id="month" style="width: 65px;padding: 8px;text-align: center;" required>
+                                                            <option value=01>01</option>
+                                                            <option value=02>02</option>
+                                                            <option value=03>03</option>
+                                                            <option value=04>04</option>
+                                                            <option value=05>05</option>
+                                                            <option value=06>06</option>
+                                                            <option value=07>07</option>
+                                                            <option value=08>08</option>
+                                                            <option value=09>09</option>
+                                                            <option value=10>10</option>
+                                                            <option value=11>11</option>
+                                                            <option value=12>12</option>
+                                                        </select>
+                                                        <label></label>
+                                                        <select name="days" id="days" style="width: 65px; padding: 8px;text-align: center;" required>
+                                                            <option value=01>01</option>
+                                                            <option value=02>02</option>
+                                                            <option value=03>03</option>
+                                                            <option value=04>04</option>
+                                                            <option value=05>05</option>
+                                                            <option value=06>06</option>
+                                                            <option value=07>07</option>
+                                                            <option value=08>08</option>
+                                                            <option value=09>09</option>
+                                                            <option value=10>10</option>
+                                                            <option value=11>11</option>
+                                                            <option value=12>12</option>
+                                                            <option value=13>13</option>
+                                                            <option value=14>14</option>
+                                                            <option value=15>15</option>
+                                                            <option value=16>16</option>
+                                                            <option value=17>17</option>
+                                                            <option value=18>18</option>
+                                                            <option value=19>19</option>
+                                                            <option value=20>20</option>
+                                                            <option value=21>21</option>
+                                                            <option value=22>22</option>
+                                                            <option value=23>23</option>
+                                                            <option value=24>24</option>
+                                                            <option value=25>25</option>
+                                                            <option value=26>26</option>
+                                                            <option value=27>27</option>
+                                                            <option value=28>28</option>
+                                                            <option value=29>29</option>
+                                                            <option value=30>30</option>
+                                                            <option value=31>31</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
-                                                <div class="form-group">
+                                            </div>
+                                            <div class="row">
+                                                <div class="form-group col-sm-6">
+                                                    <label for="phone" class="label_client">Телефон</label>
+                                                    <input placeholder="" name="phone" id="phone" type="text"
+                                                           class="form-control" value="{{ $phone }}">
+                                                </div>
+                                                <div class="form-group col-sm-6">
                                                     <label for="email" class="label_client">Почта</label>
                                                     <input placeholder="" name="email" id="email" type="text"
                                                            class="form-control" value="{{ $email }}">
                                                 </div>
-                                                <input type="hidden" name="timetableId" id="timetableId" value=""/>
-                                                <input type="hidden" name="_token" id="token"
-                                                       value="{{ csrf_token() }}">
                                             </div>
+                                            <input type="hidden" name="timetableId" id="timetableId" value=""/>
+                                            <input type="hidden" name="_token" id="token"
+                                                   value="{{ csrf_token() }}">
                                             <div id="confirm">
                                                 <div style="width: 63%; text-align: center; float: left;"><br>
                                                     <div class="btn btn-success" style="float: right;" id="send_form">
